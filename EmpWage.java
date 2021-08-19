bee5876ff4d8c0023cc037d20583b9ff4e549589
s4ur4b <saurabhchavn2110@gmail.com>
2021-08-19 19:36:07 +0530

    Added:Program to check attendance

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,27 @@
 package calculator;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class EmpWage {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage computation Program");
+        attendance object = new attendance();
+        object.selection();
+
     }
+}
+class attendance{
+        int empPresent=0;
+        int empAbsent=1;
+        void selection() {
+            int check = (int) ThreadLocalRandom.current().nextInt(0, 2);
+            System.out.println(check);
+            if (check == empPresent) {
+
+                System.out.println("Employee is Present");
+
+            } else {
+                System.out.println("Employee is Absent");
+
+            }
+        }
 }
