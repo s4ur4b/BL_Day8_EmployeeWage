814b0e2b59f557d140d95d1e6b560692f318ad6c
s4ur4b <saurabhchavn2110@gmail.com>
2021-08-19 19:51:05 +0530

    Added:Daily Employee Wage Program

--- EmpWage.java
+++ EmpWage.java
@@ -4,12 +4,11 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class EmpWage {
     public static void main(String[] args) {
-        attendance object = new attendance();
-        object.selection();
-
+            DailyWage object = new DailyWage();
+            object.selection();
     }
 }
-class attendance{
+class Attendance{
         int empPresent=0;
         int empAbsent=1;
         void selection() {
@@ -24,4 +23,26 @@ class attendance{
 
             }
         }
+}
+
+class DailyWage{
+    int empFullTime=0;
+    int wagePerHour=20;
+    int workingHour=0;
+    int wage=0;
+    void selection() {
+        int check = (int) ThreadLocalRandom.current().nextInt(0, 2);
+        System.out.println(check);
+        if (check == empFullTime) {
+                workingHour=8;
+            System.out.println("Employee is Full-time");
+            wage=wagePerHour*workingHour;
+            System.out.println("Daily wage of Employee: "+wage);
+
+
+        } else {
+            System.out.println("Employee is Absent");
+            System.out.println("Daily wage of Employee: "+wage+" Rs ");
+        }
+    }
 }
