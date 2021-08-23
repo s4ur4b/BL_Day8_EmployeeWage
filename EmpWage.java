c0606900c46d21ee32120666398a860bea2c46ca
s4ur4b <saurabhchavn2110@gmail.com>
2021-08-23 12:52:18 +0530

    Added: Employee Wage Computation for Multiple Companies

--- EmpWage.java
+++ EmpWage.java
@@ -1,69 +1,37 @@
 package calculator;
 
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class EmpWage {
+    //Main Method
     public static void main(String[] args) {
            EmployeeWageMaxHours  object = new EmployeeWageMaxHours();
-            object.selectionMax();
+           Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Company name: ");
+        String company = sc.nextLine();
+        System.out.println("Enter Employee Rate per Hour");
+        int empRatePerHour = sc.nextInt();
+        System.out.println("Enter maximum number of working Days for the Worker");
+        int workingDays = sc.nextInt();
+        System.out.println("Enter maximum number of working Hours for the Worker");
+        int maxHoursInMonth = sc.nextInt();
+            //Passing the User Input parameters into to function
+            object.selectionMax(company,empRatePerHour,workingDays,maxHoursInMonth);
     }
 }
-class Attendance{
-        int empPresent=0;
-        int empAbsent=1;
-        void selection() {
-            int check = (int) ThreadLocalRandom.current().nextInt(0, 2);
-            System.out.println(check);
-            if (check == empPresent) {
-
-                System.out.println("Employee is Present");
-
-            } else {
-                System.out.println("Employee is Absent");
-
-            }
-        }
-}
-
-class DailyWage{
-    private int empFullTime=0;
-    private int wagePerHour=20;
-    private int workingHour=0;
-    private int wage=0;
-    private int monthlyWage=0;
-    private int monthDays=20;
-    void selection() {
-        int check = (int) ThreadLocalRandom.current().nextInt(0, 2);
-        System.out.println(check);
-        switch (check){
-            case 0:     workingHour=8;
-            System.out.println("Employee is Full-time");
-            wage=wagePerHour*workingHour;
-            System.out.println("Daily wage of Employee: "+wage);
-            monthlyWage=wage*monthDays;
-                System.out.println("Monthly wage of Employee: "+monthlyWage);
-            break;
-
-            case 1:
-            workingHour=4;
-            System.out.println("Employee is Part-time");
-            wage=wagePerHour*workingHour;
-            System.out.println("Daily wage of Employee: "+wage+" Rs ");
-                monthlyWage=wage*monthDays;
-                System.out.println("Monthly wage of Employee: "+monthlyWage);
-            break;
-        }
-    }
-}
-
+//Created to Class for Employee Wage Computation
 class EmployeeWageMaxHours {
-    public static final int partTime=1, fullTime=2, empRatePerHour=20, workingDays=20, maxHoursInMonth=100;
+    public static final int partTime=1, fullTime=2;
 
-    void selectionMax(){
+    void selectionMax(String company,int empRatePerHour, int workingDays, int maxHoursInMonth){
+
         int empHours, totalWorkingDays = 0, totalEmpHours = 0;
+
         while(totalEmpHours<maxHoursInMonth && totalWorkingDays<workingDays){
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random()*10%3);
+
             switch (empCheck){
                 case partTime:
                     empHours=4;
@@ -74,10 +42,12 @@ class EmployeeWageMaxHours {
                 default:
                     empHours=0;
             }
+
             totalEmpHours += empHours;
-            System.out.println("Total working Days"+totalWorkingDays+"Total working Hours"+totalEmpHours);
+
         }
+        System.out.println("For the company" +company+"\nTotal working Days"+totalWorkingDays+"\nTotal working Hours"+totalEmpHours);
         int totalEmpWage = empRatePerHour * totalEmpHours;
-        System.out.println("Total Employee Wage: "+totalEmpWage);
+        System.out.println("Total Employee Wage: "+totalEmpWage+" Rs/-");
     }
 }
